7ecd217a5886caea8c8796ed9f7b3845db8664b9
Ivan <ivanpkhchan@gmail.com>
2018-09-23 14:34:09 -0400

    built app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,125 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RepoTable from './repoTable';
+import axios from 'axios';
+require('dotenv').config();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchResults: [],
+                  favorites: []}
+  }
+
+  componentDidMount() {
+    this.setState({favorites: (JSON.parse(localStorage.getItem("favorites")) || [])});
+  }
+
+  checkInputLength = (e) => {
+    if(!e.target.value.length) {
+      this.setState({searchResults: []});
+    }
+  }
+
+  updateLists = (favorites = [...this.state.favorites], searchResults = [...this.state.searchResults]) => {
+    const favoritesUrls = favorites.map((favorite) => {return favorite.url});
+
+    searchResults.forEach((repo) => {
+      if (favoritesUrls.indexOf(repo.url) > -1) {
+        repo.faved = true;
+      }
+      else {
+        repo.faved = false;
+      }
+    })
+
+    this.setState({searchResults: searchResults, favorites: favorites});
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+
+  }
+
+  addFavorite = (e, index) => {
+    e.preventDefault();
+    this.updateLists([...this.state.favorites, this.state.searchResults[index]]);
+  }
+
+  removeFavorite = (e, index) => {
+    e.preventDefault();
+    let faves = [...this.state.favorites];
+    faves.splice(index, 1);
+    this.updateLists(faves);
+  }
+
+  searchRepos = (e) => {
+    e.preventDefault();
+
+    axios({
+      method: 'POST',
+      url: 'https://api.github.com/graphql',
+      headers: {"Authorization": `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`},
+      data: JSON.stringify({
+       query :
+        `query {
+          search(query: ${document.querySelector('.searchInput').value}, type: REPOSITORY, first: 10) {
+        		edges {
+              node {
+                ... on Repository{
+                  name
+                  owner {
+                    login
+                  }
+                  releases(last: 1) {
+                    nodes {
+                      tag {
+                        name
+                      }
+                    }
+                  }
+                  url
+                  primaryLanguage {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }`
+      })
+    }).then((results) => {
+      let repos = [];
+
+      results.data.data.search.edges.forEach((result) => {
+        repos.push(
+          {name: result.node.name,
+           owner: result.node.owner.login,
+           primeLang: (result.node.primaryLanguage ? result.node.primaryLanguage.name : "-"),
+           lastTag: (result.node.releases.nodes[0] ? result.node.releases.nodes[0].tag.name : "-"),
+           url: result.node.url,
+           faved: false
+          }
+        )
+      })
+
+      this.updateLists(this.state.favorites, repos);
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>My Github Favorites</h1>
+        <div className="flex">
+          <div className="searchContainer">
+            <form action="#" className="searchForm flex flex-justify-space-between">
+              <input type="text" className="searchInput" onKeyUp={this.checkInputLength} />
+              <input type="submit" className="searchButton" value="Search" onClick= {this.searchRepos}/>
+            </form>
+            <RepoTable repos={this.state.searchResults} handleLink={this.addFavorite} linkText="Add"/>
+          </div>
+          <div className="favoritesContainer">
+            <RepoTable repos={this.state.favorites} handleLink={this.removeFavorite} linkText="Remove"/>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/repoTable.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+// const resultsDisplay = {
+//   margin: '40px',
+//   border: '5px solid pink'
+// };
+
+const RepoTable = (props) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th className="primeLang">Language</th>
+          <th className="lastTag">Latest tag</th>
+          <th className="emptyHeader"></th>
+        </tr>
+      </thead>
+      {(props.repos.length)
+        ? <tbody className="resultsDisplay">
+            {props.repos.map((repo, index) =>
+              <tr key={`${repo.name}/${repo.owner}`}>
+                 <td><a href={repo.url} className="repoLink" target="_blank" rel="noopener noreferrer">{repo.name}/{repo.owner}</a></td>
+                 <td className="primeLang">{repo.primeLang}</td>
+                 <td className="lastTag">{repo.lastTag}</td>
+                 {(props.linkText === "Add")
+                   ? (repo.faved === false)
+                      ? <td><a href="##" className={props.linkText} onClick={(e) => props.handleLink(e, index)}>{props.linkText}</a></td>
+                      : null
+                   : <td><a href="##" className={props.linkText} onClick={(e) => props.handleLink(e, index)}>{props.linkText}</a></td>
+                 }
+               </tr>
+            )}
+          </tbody>
+        : null
+      }
+    </table>
+  )
+}
+
+
+export default RepoTable
